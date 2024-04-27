1527368e4ac2c16c0054ceaa4dd040fdc88a9f06
Aloksingh <aloksingh@polynomial.ai>
2024-04-27 09:58:49 +0530

    Commit 1

---
+++ backend/db.js
@@ -0,0 +1,33 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const mongoURL = 'mongodb+srv://alok271992:USsjnOvIdDz6xZu4@cluster0.k7hfd2k.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0'
+
+let _db;
+
+const initdb = callback => {
+    if (_db) {
+        console.log("Database is already initialized");
+        return callback(null, _db);
+    }
+    MongoClient.connect(mongoURL)
+        .then(client => {
+            _db = client;
+            callback(null, _db)
+        })
+        .catch(err => {
+            callback(err);
+        });
+}
+
+
+const getDb = () => {
+    if (!_db) {
+        throw new Error('Database not initialized');
+    }
+    return _db;
+}
+
+module.exports = {
+    initdb, getDb
+}
